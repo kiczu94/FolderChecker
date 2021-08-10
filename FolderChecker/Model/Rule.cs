16c9d898dc09223e058771ea8ca3b47a80efc5a8
Tomasz Tkocz <tkoczt@gmail.com>
2021-08-10 08:13:46 +0100

    Initial commit

---
+++ FolderChecker/Model/Rule.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderChecker.Model
+{
+    public class Rule
+    {
+        private string _ruleName;
+        private List<string> _MailAdresses = new List<string>();
+        private string _pathToTrack;
+        public List<string> myMailAdresses
+        {
+            get
+            {
+                return myMailAdresses;
+            }
+        }
+        public string myPathToTrack
+        {
+            get
+            {
+                return _pathToTrack;
+            }
+
+            set
+            {
+                _pathToTrack = value;
+            }
+        }
+        public string myRuleName
+        {
+            get
+            {
+                return _ruleName;
+            }
+            set
+            {
+                _ruleName = value;
+            }
+        }
+        public Rule(string name,string pathToTrack, List<string> mailAdresses)
+        {
+            _ruleName = name;
+            _pathToTrack = pathToTrack;
+            _MailAdresses = mailAdresses;
+        }
+        public Rule(string name,string pathToTrack)
+        {
+            _ruleName = name;
+            _pathToTrack = pathToTrack;
+        }
+        public Rule()
+        {
+
+        }
+        public void AddEmailAdress(string emailAdress)
+        {
+            _MailAdresses.Add(emailAdress);
+        }
+    }
+}
---
+++ FolderChecker/ViewModel/MainViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FolderChecker.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private List<Model.Rule> _rules;
+
+        public List<Model.Rule> MyRules
+        {
+            get { return _rules; }
+            set
+            {
+                _rules = value;
+                OnPropertyChanged();
+            }
+        }
+        private List<string> _rulesNames;
+
+        public List<string> MyRulesNames
+        {
+            get { return _rulesNames; }
+            set { _rulesNames = value; }
+        }
+
+        public MainViewModel()
+        {
+            MyRules = GetRule();
+            _rulesNames = GetRuleNames();
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private List<Model.Rule> GetRule()
+        {
+            List<Model.Rule> randomList = new List<Model.Rule>();
+            randomList.Add(new Model.Rule("zasada 1", "ścieżka1"));
+            randomList.Add(new Model.Rule("zasada 2", "ścieżka2"));
+            randomList.Add(new Model.Rule("zasada 3", "ścieżka3"));
+            randomList.Add(new Model.Rule("zasada 4", "ścieżka4"));
+            randomList.Add(new Model.Rule("zasada 5", "ścieżka5"));
+            randomList.Add(new Model.Rule("zasada 6", "ścieżka6"));
+            return randomList;
+        }
+        private List<string> GetRuleNames()
+        {
+            List<string> ruleNames = new List<string>();
+            foreach (var rule in _rules)
+            {
+                ruleNames.Add(rule.myRuleName);
+            }
+            return ruleNames;
+        }
+    }
+}
