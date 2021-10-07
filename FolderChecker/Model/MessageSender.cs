69af5365a069404cb783c51af176aca16420247e
Tomasz Tkocz <tkoczt@gmail.com>
2021-10-07 13:58:06 +0200

    New functionality: Gathering messages to send them together

--- FolderChecker/Model/MessageSender.cs
+++ FolderChecker/Model/MessageSender.cs
@@ -6,11 +6,13 @@ using MailKit.Net.Smtp;
 using MimeKit;
 using System.Windows;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace FolderChecker.Model
 {
     public class MessageSender
     {
+        private bool alreadyWaiting=false;
         private List<MimeMessage> mimeMessages;
         private string _emailAdress;
         private string _password;
@@ -37,39 +39,15 @@ namespace FolderChecker.Model
         {
             if (_password != null && _emailAdress != null)
             {
-                SendMessage(GetMimeMessage(source, args, CreateText(args)));
+                mimeMessages.Add(GetMimeMessage(source, args, CreateText(args)));
+                if (alreadyWaiting==false)
+                {
+                    SendMessages();
+                }
             }
             else
             {
                 MessageBox.Show("Nie wysłano maila- brak hasła i maila");
-            }
-        }
-        private void SendMessage(MimeMessage message)
-        {
-            SmtpClient smtp = new SmtpClient();
-            try
-            {
-                smtp.Connect("smtp.gmail.com", 465, true);
-                smtp.Authenticate(_emailAdress, _password);
-                smtp.Send(message);
-            }
-            catch (Exception ex)
-            {
-                //MetadataToken of an exception when adress email is uncorrect
-                if (ex.TargetSite.MetadataToken==100665740|| ex.TargetSite.MetadataToken == 100665727)
-                {
-                    MessageBox.Show($"Adres e-mail {message.To} nieprawidłowy");
-                }
-                else
-                {
-                    MessageBox.Show(ex.Message);
-                }
-
-            }
-            finally
-            {
-                smtp.Disconnect(true);
-                smtp.Dispose();
             }
         }
         private string CreateText(WatcherInvokedEventArgs args)
@@ -81,12 +59,13 @@ namespace FolderChecker.Model
                     textToreturn = $"Utworzono nowy plik {args.Name} w folderze {GetFolderPath(args)}";
                     break;
                 case WatcherChangeTypes.Renamed:
-                    textToreturn = $"Zmieniono nazwę pliku z {args.OldName.Remove(0,args.OldName.LastIndexOf('\\')+1)} na {args.Name.Remove(0, args.Name.LastIndexOf('\\') + 1)} w folderze {GetFolderPath(args)}";
+                    textToreturn = $"Zmieniono nazwę pliku z {args.OldName.Remove(0, args.OldName.LastIndexOf('\\') + 1)} na {args.Name.Remove(0, args.Name.LastIndexOf('\\') + 1)} w folderze {GetFolderPath(args)}";
                     break;
                 case WatcherChangeTypes.Deleted:
                     textToreturn = $"Usunięto plik {args.Name.Remove(0, args.Name.LastIndexOf('\\') + 1)} w folderze {GetFolderPath(args)}";
                     break;
                 case WatcherChangeTypes.Changed:
+                    textToreturn = "1";
                     break;
             }
             return textToreturn;
@@ -147,6 +126,54 @@ namespace FolderChecker.Model
                 smtp.Dispose();
             }
             return isEmailAndPasswordCorrect;
+        }
+        public async void SendMessages()
+        {
+            alreadyWaiting = true;
+            await Task.Delay(20000);
+
+            foreach (var message in mimeMessages)
+            {
+                SmtpClient smtp = new SmtpClient();
+                try
+                {
+                    smtp.Connect("smtp.gmail.com", 465, true);
+                    smtp.Authenticate(_emailAdress, _password);
+                    smtp.Send(message);
+                }
+                catch (Exception ex)
+                {
+                    //MetadataToken of an exception when adress email is uncorrect
+                    if (ex.TargetSite.MetadataToken == 100665740 || ex.TargetSite.MetadataToken == 100665727)
+                    {
+                        MessageBox.Show($"Adres e-mail {message.To} nieprawidłowy");
+                    }
+                    else
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                finally
+                {
+                    smtp.Disconnect(true);
+                    smtp.Dispose();
+                }
+            }
+            mimeMessages.Clear();
+        }
+        private void CreateListOfEmail()
+        {
+            List<string> emailAdresses = new List<string>();
+            foreach (var message in mimeMessages)
+            {
+                foreach (var emailAdress in message.To)
+                {
+                    if (!emailAdresses.Contains(emailAdress.ToString()))
+                    {
+                        emailAdresses.Add(emailAdress.ToString());
+                    }
+                }
+            }
         }
     }
 }
